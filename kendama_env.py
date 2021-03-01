3df14c68b43aa84c0c4b9ad2677b3337d4e20bfd
Arnaud Chol <arnaud.chol@gmail.com>
2021-03-01 12:03:19 +0100

    Revert "Merge remote-tracking branch 'origin/alexis' into main"
    This reverts commit 9598da060570dee8d41f20312d9bba7891ea5eac.

--- kendama_env.py
+++ kendama_env.py
@@ -4,10 +4,6 @@ import pybullet as p
 import time
 import pybullet_data
 import numpy as np
-import scipy.spatial.transform.Rotation as R
-
-TIME_LIM = 300
-X_LIM, Y_LIM, Z_LIM = 10, 10, 10
 
 class KendamaEnv(gym.Env):
   """Custom Environment that follows gym interface"""
@@ -15,15 +11,14 @@ class KendamaEnv(gym.Env):
 
   def __init__(self):
     super(KendamaEnv, self).__init__()
-
+    self.dt = 240.0
     # 2 (force, torque) * 3D
-    self.action_space = spaces.Box(low=-1, high=1, shape=(2,3), dtype=np.float32)
+    self.action_space = spaces.Box(low=-1, high=1,
+                                        shape=(2,3), dtype=np.float32)
     
     # 2 objects * 3 (pos, vit, acc) * 3D
-    self.observation_space = spaces.Box(low=-1, high=1, shape=(2,3,3), dtype=np.float32)
-
-    # time during the simulation
-    self.time = 0
+    self.observation_space = spaces.Box(low=-1, high=1,
+                                        shape=(2,3,3), dtype=np.float32)
 
     self.physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
     p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
@@ -37,7 +32,7 @@ class KendamaEnv(gym.Env):
     cubeStartPos = [0,0,0.35]
     cubeStartOrientation = p.getQuaternionFromEuler([0,3.1415/2.0,0])
     cubeStartPos = [0,0,0.5]
-    cubeStartOrientation = p.getQuaternionFromEuler([3.14,0,0])
+    cubeStartOrientation = p.getQuaternionFromEuler([0,3.14/2.0,0])
     self.dama = p.loadURDF("./URDF/kendama_ball/kendama_ball.urdf",cubeStartPos, cubeStartOrientation, 
                     # useMaximalCoordinates=1, ## New feature in Pybullet
                     flags=p.URDF_USE_INERTIA_FROM_FILE)
@@ -53,32 +48,12 @@ class KendamaEnv(gym.Env):
     
     p.createConstraint(self.ken, -1, self.center, -1, p.JOINT_POINT2POINT,jointAxis=[1,0,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
     self.pulling = True
-    self.pulling2 = False
     self.link = p.createConstraint(self.center, -1, self.dama, -1, p.JOINT_POINT2POINT,jointAxis=[1,1,1],parentFramePosition=[0,0,0],childFramePosition=[0.5,0,0])
     p.changeConstraint(self.link,maxForce=10000)
 
-  # ckecks time limit
-  def out_of_time(self):
-    return self.time > TIME_LIM
-  
-  # check spatial limits
-  def out_of_box(self, observation):
-    kx, ky, kz = np.absolute(observation[0,0])
-    dx, dy, dz = np.absolute(observation[1,0])
-
-    if ( kx > X_LIM or dx > X_LIM):
-      return False
-    if ( ky > Y_LIM or dy > Y_LIM):
-      return False
-    if ( kz > Z_LIM or dz > Z_LIM):
-      return False
-
-    return True
 
 
   def step(self, action):
-    self.time += 1
-
     # tension 
     if(self.pulling):
         force = p.getConstraintState(self.link)
@@ -87,93 +62,44 @@ class KendamaEnv(gym.Env):
     #print("({:.2f} {:.2f} {:.2f})".format(force[0], force[1], force[2]))
     a = np.array(p.getBasePositionAndOrientation(self.center)[0])
     a -= np.array(p.getBasePositionAndOrientation(self.dama)[0])
-    # compute the work of the force
     tension = np.dot(a,np.array(force))
 
-    # update the parameters : wire not under tension anymore
     if(tension < 0 and self.pulling):
         self.pulling = False
         p.removeConstraint(self.link)
-    # wire back under tension
     if(np.linalg.norm(a) > 0.5 and not self.pulling):
         self.pulling = True
-        self.pulling2 = True
         axis = np.dot(np.linalg.inv(np.reshape(p.getMatrixFromQuaternion(p.getBasePositionAndOrientation(self.dama)[1]), [3,3])), a/np.linalg.norm(a))
         self.link = p.createConstraint(self.center, -1, self.dama, -1, p.JOINT_POINT2POINT,axis/2.0, [0, 0, 0],axis/2.0)
         p.changeConstraint(self.link,maxForce=10000)
 
 
 
-    kenPos,_ = p.getBasePositionAndOrientation(self.ken)
+    pos_ken,_ = p.getBasePositionAndOrientation(self.ken)
     p.applyExternalForce(objectUniqueId=self.ken, linkIndex=-1,
-                         forceObj=action[0], posObj=kenPos, flags=p.WORLD_FRAME)
+                         forceObj=action[0], posObj=pos_ken, flags=p.WORLD_FRAME)
     p.applyExternalTorque(objectUniqueId=self.ken, linkIndex=-1,
                          torqueObj=action[1], flags=p.WORLD_FRAME)
     p.stepSimulation()
-
-    # get the parameters after the simulation
-    kenPos, kenOr = p.getBasePositionAndOrientation(self.ken)
-    kenOr = p.getEulerFromQuaternion(kenOr)
-    kenV, kenW = p.getBaseVelocity(self.ken)
+    kenVel,kenVelRad = p.getBaseVelocity(self.ken)
     kenAcc = tuple(np.subtract(kenVel, self.v_ken) / (self.dt*1000))
     kenAccRad = tuple(np.subtract(kenVelRad, self.vRad_ken) / (self.dt*1000))
     self.v_ken = kenVel
     self.vRad_ken = kenAccRad
-
-    damaPos, damaOr = p.getBasePositionAndOrientation(self.dama)
-    damaOr = p.getEulerFromQuaternion(damaOr)
-    damaV, damaW = p.getBaseVelocity(self.dama)
+    kenPos,kenAngle = p.getBasePositionAndOrientation(self.ken)
+    damaVel,damaVelRad = p.getBaseVelocity(self.dama)
     damaAcc = tuple(np.subtract(damaVel, self.v_dama) / (self.dt*1000))
     damaAccRad = tuple(np.subtract(damaVelRad, self.vRad_dama) / (self.dt*1000))
     self.v_dama = damaVel
     self.vRad_dama = damaAccRad
-    
-    observation = np.array([
-      [kenPos, kenOr, kenV, kenW, kenAcc, kenAccRad],
-      [damaPos, damaOr, damaV, damaW, damaAcc, damaAccRad]
-      ])
-
+    damaPos,damaAngle = p.getBasePositionAndOrientation(self.dama)
+    observation = np.array([[kenPos,kenVel,kenAcc,kenAngle,kenVelRad,kenAccRad],[damaPos, damaVel, damaAcc,damaAngle,damaVelRad,damaAccRad]])
+    reward = 1
     done = False
     info = False
-
-    # get 
-    localOrientation = [0,0,1]
-    r = R.from_quat(p.getBasePositionAndOrientation[1])
-    localOrientation = r.apply(localOrientation)
-
-    # add final reward
-    if self.pulling:
-        reward = 
-          damaV[2] +
-          np.dot(damaV[2],localOrientation[2])**2 -
-          lambda_ * (kenPos[2]**2 + np.dot(kenVel, kenVel))
-    else:
-      reward = 
-        (kenPos[0] - damaPos[0])**2 +
-        (damaV[:2] - kenOr[:2])**2 + (kenPos[1] - damaPos[1])**2 +
-        (kenOr[0] - damaOr[0])**2 + (kenOr[1] - damaOr[1])**2
-
-    # dama is back beneath the ken, then it is a final state
-    if self.pulling2 and damaPos[2] < kenpos[2]:
-      done = True
-      final_reward = reward_list[-1]
-
-    if good_collision # Condition sur la distance entre le centre du ken et le centre du dama : a determiner
-      reward = 100
-  # Il faut normaliser le return a chaque fois ... : se fait avec les fonctions exponentielles
-
-    if out_of_box():
-      done = True
-      reward = 0
-
-    if self.out_of_time():
-      done = True
-      reward = 0
-    
     return observation, reward, done, info
 
   def reset(self):
-    self.time = 0
     kenVel = [0,0,0]
     kenAcc = [0,0,0]
     kenPos = [0,0,0]
